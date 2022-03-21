5c6765893172e9e9ae3c1a76d96bc4a035f69a5c
Aidan Hall <aidan.hall@outlook.com>
2022-03-21 17:11:21 +0000

    [Main.hs] Maths expression evaluator.
    Comes with automatic type conversion.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
@@ -62,9 +62,14 @@ brackets = between (symbol "[") (symbol "]")
 braces :: Parser a -> Parser a
 braces = between (symbol "{") (symbol "}")
 
+data Value
+  = Integer Int
+  | Float Float
+  deriving (Eq, Ord, Show)
+
 data Expr
-  = Var String
-  | Int Int
+  = Value Value
+  | Variable String
   | Negation Expr
   | Sum Expr Expr
   | Subtr Expr Expr
@@ -74,26 +79,37 @@ data Expr
   deriving (Eq, Ord, Show)
 
 -- | Parses integers in hexadecimal, octal or decimal format, based on a prefix.
-pInteger :: Parser Expr
+pInteger :: Parser Value
 pInteger =
-  Int
+  Integer
     <$> lexeme
-      ( (string "0x" *> L.hexadecimal)
-          <|> (string "0o" *> L.octal)
-          <|> L.decimal
+      ( choice
+          [ string "0x" *> L.hexadecimal,
+            string "0o" *> L.octal,
+            L.decimal
+          ]
+          <?> "integer"
       )
+
+pNumber :: Parser Value
+pNumber =
+  try floating <|> integer
+  where
+    floating = Float <$> lexeme L.float
+    integer = pInteger
 
 pVariable :: Parser Expr
 pVariable =
-  Var
+  Variable
     <$> lexeme
       ((:) <$> letterChar <*> many alphaNumChar <?> "variable")
 
 pTerm :: Parser Expr
-pTerm = choice
-  [ parens pExpr
-    , pVariable
-    , pInteger
+pTerm =
+  choice
+    [ parens pExpr,
+      pVariable,
+      Value <$> pNumber
     ]
 
 pExpr :: Parser Expr
@@ -101,21 +117,58 @@ pExpr = makeExprParser pTerm operatorTable
 
 operatorTable :: [[Operator Parser Expr]]
 operatorTable =
- [ [ prefix "-" Negation
-    , prefix "+" id
-    ]
-  , [ binary "*" Product
-    , binary "/" Division
-    , binary "%" Modulo
-    ]
-  , [ binary "+" Sum
-    , binary "-" Subtr
+  [ [ prefix "-" Negation,
+      prefix "+" id
+    ],
+    [ binary "*" Product,
+      binary "/" Division,
+      binary "%" Modulo
+    ],
+    [ binary "+" Sum,
+      binary "-" Subtr
     ]
   ]
 
 binary :: Text -> (Expr -> Expr -> Expr) -> Operator Parser Expr
-binary  name f = InfixL  (f <$ symbol name)
+binary name f = InfixL (f <$ symbol name)
 
 prefix, postfix :: Text -> (Expr -> Expr) -> Operator Parser Expr
-prefix  name f = Prefix  (f <$ symbol name)
+prefix name f = Prefix (f <$ symbol name)
 postfix name f = Postfix (f <$ symbol name)
+
+eval :: Expr -> Value
+eval = \case
+  Value v -> v
+  Variable _ -> undefined
+  Negation x ->
+    case eval x of
+      Integer n -> Integer $ negate n
+      Float n -> Float $ negate n
+  Sum x y ->
+    case (eval x, eval y) of
+      (Integer x', Integer y') -> Integer $ x' + y'
+      (Float x', Float y') -> Float $ x' + y'
+      (Integer x', Float y') -> Float $ fromIntegral x' + y'
+      (Float x', Integer y') -> Float $ x' + fromIntegral y'
+  Subtr x y ->
+    case (eval x, eval y) of
+      (Integer x', Integer y') -> Integer $ x' - y'
+      (Float x', Float y') -> Float $ x' - y'
+      (Integer x', Float y') -> Float $ fromIntegral x' - y'
+      (Float x', Integer y') -> Float $ x' - fromIntegral y'
+  Product x y ->
+    case (eval x, eval y) of
+      (Integer x', Integer y') -> Integer $ x' * y'
+      (Float x', Float y') -> Float $ x' * y'
+      (Integer x', Float y') -> Float $ fromIntegral x' * y'
+      (Float x', Integer y') -> Float $ x' * fromIntegral y'
+  Division x y ->
+    case (eval x, eval y) of
+      (Integer x', Integer y') -> Integer $ x' `div` y'
+      (Float x', Float y') -> Float $ x' / y'
+      (Integer x', Float y') -> Float $ fromIntegral x' / y'
+      (Float x', Integer y') -> Float $ x' / fromIntegral y'
+  Modulo x y ->
+    case (eval x, eval y) of
+      (Integer x', Integer y') -> Integer $ x' `mod` y'
+      (_, _) -> error "Modulo must be between two integers."
