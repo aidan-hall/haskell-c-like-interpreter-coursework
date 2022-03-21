67599b1a3b34626173ba1cdfafedcb1f4f6beb15
Aidan Hall <aidan.hall@outlook.com>
2022-03-21 13:39:30 +0000

    [Main.hs] Removed tutorial accretion.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Lib
 import Text.Megaparsec
+
 import Text.Megaparsec.Debug
 import Text.Megaparsec.Char
 import Control.Monad
@@ -14,39 +15,6 @@ import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
 
-data Scheme
-  = SchemeData
-  | SchemeFile
-  | SchemeFtp
-  | SchemeHttps
-  | SchemeHttp
-  | SchemeIrc
-  | SchemeMailto
-  deriving (Eq, Show)
-
-pScheme :: Parser Scheme
-pScheme = choice
-  [ SchemeData   <$ string "data"
-  , SchemeFile   <$ string "file"
-  , SchemeFtp    <$ string "ftp"
-  , SchemeHttps  <$ string "https"
-  , SchemeHttp   <$ string "http"
-  , SchemeIrc    <$ string "irc"
-  , SchemeMailto <$ string "mailto" ]
-
-data Uri = Uri
-  { uriScheme :: Scheme
-  , uriAuthority :: Maybe Authority
-  , uriPath :: FilePath
-  , uriQuery :: Maybe Text
-  , uriFragment :: Maybe Text
-  } deriving (Eq, Show)
-
-data Authority = Authority
-  { authUser :: Maybe (Text, Text) -- (user, password)
-  , authHost :: Text
-  , authPort :: Maybe Int
-  } deriving (Eq, Show)
 
 {- This is the main entry point to your program. -}
 main :: IO ()
@@ -90,28 +58,3 @@ lexeme = L.lexeme sc
 
 symbol :: Text -> Parser Text
 symbol = L.symbol sc
-
-pUri :: Parser Uri
-pUri = do
-  uriScheme <- dbg "scheme" pScheme <?> "valid scheme"
-  void (char ':')
-  uriAuthority <- dbg "auth" . optional $ do            -- (1)
-    void (string "//")
-    authUser <- dbg "user" . optional . try $ do              -- (2)
-      user <- T.pack <$> some alphaNumChar       -- (3)
-      void (char ':')
-      password <- T.pack <$> some alphaNumChar
-      void (char '@')
-      return (user, password)
-    authHost <- T.pack <$> dbg "host" (some (alphaNumChar <|> char '.'))
-    authPort <- dbg "port" $ optional (char ':' *> L.decimal) -- (4)
-    return Authority {..}                        -- (5)
-  void $ optional (char '/')
-  uriPath <- many (alphaNumChar <|> char '/')
-  uriQuery <- optional $ do
-    void (char '?')
-    T.pack <$> many alphaNumChar
-  uriFragment <- optional $ do
-    void (char '#')
-    T.pack <$> many alphaNumChar
-  return Uri {..}                                -- (6)
