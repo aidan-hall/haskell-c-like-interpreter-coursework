f532f1ded0c6979b8fbd90f544d1355e3ddf68b8
Aidan Hall <aidan.hall@outlook.com>
2022-03-22 19:58:50 +0000

    [Statement.hs] Added branching parsing.

--- src/Statement.hs
+++ src/Statement.hs
@@ -20,6 +20,9 @@ data Statement
   = Expr Expr
   | Assign Assignment
   | Block [Statement]
+  | IfElse Expr Statement Statement
+  | If Expr Statement
+  | While Expr Statement
   deriving (Show, Eq)
 
 data Assignment = Assignment
@@ -35,9 +38,29 @@ pAssign = do
   value <- pExpr
   pure Assignment {..}
   
+pIf :: Parser Statement
+pIf = do
+  void $ lexeme (string "if")
+  cond <- lexeme $ parens pExpr
+  If cond <$> pStatement
+
+pIfElse :: Parser Statement
+pIfElse = do
+  If cond whenTrue <- pIf
+  void $ lexeme (string "else")
+  IfElse cond whenTrue <$> pStatement
+
+pWhile :: Parser Statement
+pWhile = do
+  void $ lexeme (string "while")
+  cond <- lexeme $ parens pExpr
+  While cond <$> pStatement
 
 pStatement :: Parser Statement
 pStatement =
   Block <$> braces (many pStatement)
+  <|> try pIfElse
+  <|> pIf
+  <|> pWhile
   <|> try (Assign <$> (pAssign <* semicolon))
   <|> Expr <$> pExpr <* semicolon
