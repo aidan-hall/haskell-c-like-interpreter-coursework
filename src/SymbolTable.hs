b2e988cc3a933bccf60f7d768891aeb7e5a0c2a6
Aidan Hall <aidan.hall@outlook.com>
2022-03-24 10:37:26 +0000

    [SymbolTable] Code golf.

--- src/SymbolTable.hs
+++ src/SymbolTable.hs
@@ -1,39 +1,35 @@
 {-# LANGUAGE RecordWildCards #-}
+
 module SymbolTable where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Types
-import Function
+import Types (Function, Value)
 
 data SymbolTable = SymbolTable
-  { symbols :: [Map String Value]
-  , functions :: Map String Function
+  { symbols :: [Map String Value],
+    functions :: Map String Function
   }
-  deriving Show
+  deriving (Show)
 
 findSymbol :: String -> SymbolTable -> Maybe Value
-findSymbol name SymbolTable {symbols = (m : ms), .. } =
+findSymbol name SymbolTable {symbols = []} = Nothing
+findSymbol name SymbolTable {symbols = (m : ms), ..} =
   case Map.lookup name m of
     Just v -> Just v
-    Nothing -> findSymbol name SymbolTable {symbols = ms, .. }
-findSymbol name SymbolTable {symbols = []} = Nothing
-
+    Nothing -> findSymbol name SymbolTable {symbols = ms, ..}
 
 addSymbol :: String -> Value -> SymbolTable -> SymbolTable
--- No scopes: can't add value
+addSymbol _ _ SymbolTable {symbols = [], ..} =
+  SymbolTable {symbols = [], ..}
 addSymbol name value SymbolTable {..} =
-  case symbols of
-    [] -> SymbolTable [] functions
-    (m:ms) -> case findSymbol name SymbolTable {..} of
-      Nothing -> SymbolTable {symbols = Map.insert name value m : ms, ..}
-      Just _ ->
-        let
-          -- Replace the symbol at the 'deepest' scope: typical shadowing behaviour.
-          replaceExisting name value [] = []
-          replaceExisting name value (m:ms) =
-            case Map.lookup name m of
-              Nothing -> m : replaceExisting name value ms
-              Just _ -> Map.insert name value m : ms
-        in
-          SymbolTable {symbols = replaceExisting name value (m : ms), ..}
+  case findSymbol name SymbolTable {..} of
+    Nothing -> SymbolTable {symbols = Map.insert name value m : ms, ..}
+      where (m:ms) = symbols
+    Just _ -> SymbolTable {symbols = replaceExisting name value symbols, ..}
+      where
+        replaceExisting name value [] = []
+        replaceExisting name value (m : ms) =
+          case Map.lookup name m of
+            Nothing -> m : replaceExisting name value ms
+            Just _ -> Map.insert name value m : ms
